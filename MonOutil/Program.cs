fee287a11565cdd8a271a8544f7bbab625060177
Trust <samuel.brule@LCIeducation.net>
2023-09-21 11:30:24 -0400

    Ajoute programme

---
+++ MonOutil/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Quel est votre nom?");
+
+string line = Console.ReadLine();
+
+Console.WriteLine("Bienvenue " + line + " a l'application!!");
+
+
+Console.WriteLine("Entrez un temperature en Celsius");
+double temperatureEnCelcius = Convert.ToDouble(Console.ReadLine());
+double temperatureEnFahrenheit = temperatureEnCelcius * 9 / 5 + 32;
+
+Console.WriteLine($"Votre temperature en fahrneheit est {temperatureEnFahrenheit}");
